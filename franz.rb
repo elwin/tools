60c20bdc065e4d8a4f51caf3764d6a8c32c228e2
elwin <elwin.stephan@gmail.com>
2023-07-24 10:43:40 +0200

    Brew formula update for franz version v1.0.4

--- franz.rb
+++ franz.rb
@@ -5,20 +5,20 @@
 class Franz < Formula
   desc "Your Swiss-Army Knife tool for interacting with Kafka."
   homepage "https://github.com/elwin/franz"
-  version "1.0.3"
+  version "1.0.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/elwin/franz/releases/download/v1.0.3/franz_Darwin_arm64.tar.gz"
-      sha256 "b4efb98e35e17557a00fc72bbf90104fb01452c00567544dd2729707cdfd8e38"
+      url "https://github.com/elwin/franz/releases/download/v1.0.4/franz_Darwin_arm64.tar.gz"
+      sha256 "02a98050667201934dde3fbcb1b67b06dc1aed72b1140845649b4e2d1ca76ed6"
 
       def install
         bin.install "franz"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/elwin/franz/releases/download/v1.0.3/franz_Darwin_x86_64.tar.gz"
-      sha256 "f85e4d61de1d676f57b5a25bfed7dbc8f34a0bf3864504a284ea3b8d8d1813e4"
+      url "https://github.com/elwin/franz/releases/download/v1.0.4/franz_Darwin_x86_64.tar.gz"
+      sha256 "fac5ebc11cc6fcc94be02f518b875b00fe51a5d2515f12e3ddb07ae30e60da37"
 
       def install
         bin.install "franz"
@@ -28,16 +28,16 @@ class Franz < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/elwin/franz/releases/download/v1.0.3/franz_Linux_arm64.tar.gz"
-      sha256 "fb37672f1dcfeac4906221f948251d117c8cfd89a76c9ce2de1188e5a21fdef9"
+      url "https://github.com/elwin/franz/releases/download/v1.0.4/franz_Linux_arm64.tar.gz"
+      sha256 "0c40b7c4e0b8ea7562bb55899fdeb252272e297fadfab1b83ea44ead27e7c9a9"
 
       def install
         bin.install "franz"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/elwin/franz/releases/download/v1.0.3/franz_Linux_x86_64.tar.gz"
-      sha256 "2490ad502c62c4bda6497b83a86b4fb95785ad65bad053302b3c40b94b5ba085"
+      url "https://github.com/elwin/franz/releases/download/v1.0.4/franz_Linux_x86_64.tar.gz"
+      sha256 "a3f6ef9fed118e688d9db90c7082cc1abafd9804939f4f9fa3e35c4717c2bb86"
 
       def install
         bin.install "franz"
